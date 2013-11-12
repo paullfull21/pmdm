53a4c213d2e4701901c72cba79d1ee40f10f1ec3
miguelcampos <camposmiguel@gmail.com>
2013-11-12 09:56:40 +0100

    EjemploReceptorAnuncios
    Ejemplo de uso de un BroadcastReceiver para la identificación de
    llamadas entrantes

---
+++ EjemploServicioMetodoPropio/src/com/dam/example/ejemplousoservicios/ServicioMusica.java
@@ -0,0 +1,54 @@
+package com.dam.example.ejemplousoservicios;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.widget.Toast;
+
+public class ServicioMusica extends Service {
+	MediaPlayer reproductor;
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		return null;
+	}
+
+	@Override
+	public void onCreate() {
+		reproductor = MediaPlayer.create(this, R.raw.one_direction);
+	}
+
+	@Override
+	public void onDestroy() {
+		reproductor.stop();
+	}
+
+	/*
+	 * El método onStart es utilizado si trabajamos con versiones inferiores a
+	 * la 2.0
+	 * 
+	 * @Override public void onStart(Intent intent, int startId) {
+	 * reproductor.start(); }
+	 */
+
+	/*
+	 * El método onStartCommand se definió a partir de la API 5
+	 * 
+	 * @see android.app.Service#onStartCommand(android.content.Intent, int, int)
+	 */
+
+	@Override
+	public int onStartCommand(Intent intent, int flags, int idArranque) {
+		if(reproductor.isPlaying()){
+		    reproductor.pause();
+		} else {
+			Toast.makeText(this, "Servicio arrancado " + idArranque,
+					Toast.LENGTH_SHORT).show();
+			reproductor.start();
+		}
+		
+		return START_STICKY;
+	}
+
+}
